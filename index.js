ce45e9feb102cd2fb7dd2e6608f31cec6bcc6d2c
Joel Feddes <joelfeddes@hotmail.com>
2021-10-01 12:33:56 -0500

    added file

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var http = require('http');
+const { networkInterfaces } = require('os');
 var url = require('url');
 
 const server = http.createServer((request, response) => {
@@ -7,14 +8,21 @@ const server = http.createServer((request, response) => {
 
     // Standard Hello World.
     response.writeHead(200, {'Content-Type': 'text/html'});
-    response.write("<h3>Hello World!</h3>")
+
+    response.write("<h1>Joel Feddes</h1>")
+    response.write("<h3>Web based Assignments:</h3>")
+    response.write("<a href='https://purple-beach-003a02a10.azurestaticapps.net/'>Getting to Know Eachother</a><br>")
+    response.write("<a href='https://gray-rock-0f51c6610.azurestaticapps.net/'>Resume</a><br>")
+    response.write("<a href='https://calm-sea-0c9111010.azurestaticapps.net/'>Dice Game</a><br>")
+    response.write("<a href='https://gray-rock-0f51c6610.azurestaticapps.net/regex'>Regex</a><br>")
+    response.write("<a href='https://gray-rock-0f51c6610.azurestaticapps.net/'>Resume</a><br>")
 
 
     // Show the url. 
     response.write("req.url="+request.url+"<br><br>");
 
     // Close the response
-    response.end('<h3>Tis the end</h3>');
+    response.end("<h3>'That's all for now. Adding more localStorage.'</h3>");
 });
 
 const port = process.env.PORT || 1337;
